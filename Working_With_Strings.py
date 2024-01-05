43626d09780f5d76d4bd76ef3146333a8031c921
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-05 03:55:43 -0500

    Update Working_With_Strings.py

--- Working_With_Strings.py
+++ Working_With_Strings.py
@@ -11,7 +11,7 @@ print('jimmy is a good boy'.count('i', 1)) # Count from index i to end of string
 print()
 
 print('This is a string'.count('s', 4, 10)) # Count from index 4 to 10-1
-print('This is a string'.count('T')) # count 'T' case sensitive, only 1 T
+print('This is a string'.count('T')) # Count 'T' case sensitive, only 1 T
 print()
 
 #endswith(suffix, [start], [end]) , case sensitive
