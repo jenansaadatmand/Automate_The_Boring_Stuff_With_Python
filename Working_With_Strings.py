57fabe0eaef56dc2d5ddb3340229d6c40e57ca2a
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-05 04:02:58 -0500

    Update Working_With_Strings.py

--- Working_With_Strings.py
+++ Working_With_Strings.py
@@ -58,7 +58,7 @@ alphanumeric and there is at least one character, false otherwise.
 Alphanumeric does not include whitespaces'' 
 
 print('abcd1234'.isalnum())
-print('a b c d 1 2 3 4'.isalnum()) # has white space
+print('a b c d 1 2 3 4'.isalnum()) # Has white space
 print('abcd'.isalnum())
 print(''.isalnum()) # no alphanumeric and all white space
 print('1234'.isalnum())
