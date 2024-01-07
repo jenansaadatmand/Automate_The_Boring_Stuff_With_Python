51d799a991f6826228f039be82d01b3351b5cd6c
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-06 19:26:03 -0500

    Update guess_the_numbers.py

--- guess_the_numbers.py
+++ guess_the_numbers.py
@@ -3,6 +3,7 @@
 import random
 secretNumber = random.randint(1, 20)
 print('I am thinking of a number between 1 and 20.')
+
 # Ask the player to guess 6 times.
 for guessesTaken in range(1, 6):
     print('Take a guess.')
