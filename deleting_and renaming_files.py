17cd5c90646a2c11c9b07b2eff9f173c645c3b99
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-09 11:11:03 -0500

    Add files via upload

---
+++ deleting_and renaming_files.py
@@ -0,0 +1,25 @@
+# deleting and renaming files, remove(), rename()
+# these two functions must be imported from the os module
+# remove('filename') deletes a file
+#rename('old name', 'new name') rename old file to new file
+# os function directory operations, deleting, moving aroud, changing things
+import os
+import time
+curDir = os.getcwd() # cwd current working directory
+print(curDir) # displays /Users/jenansaadatmand/Documents
+os.mkdir('newDir') # make new directory 
+time.sleep(2) # so we can see the action happening
+os.rename('newDir', 'newDir2') #rename directory
+
+
+
+
+
+#time.sleep(2)
+#os.rmdir('newDir2') # remove directory
+
+
+#rename('myfile.txt', 'nfile.txt')
+#file = open(nfile.text)
+#print(file)
+#file.close()
