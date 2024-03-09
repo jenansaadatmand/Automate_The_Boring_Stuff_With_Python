ce0814d17a789af171423b0d4ddf49445937c9f6
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-09 11:12:01 -0500

    Update deleting_and renaming_files.py

--- deleting_and renaming_files.py
+++ deleting_and renaming_files.py
@@ -2,7 +2,7 @@
 # These two functions must be imported from the os module
 # Remove('filename') deletes a file
 # Rename('old name', 'new name') rename old file to new file
-# os function directory operations, deleting, moving aroud, changing things
+# os function directory operations, deleting, moving around, changing things
 import os
 import time
 curDir = os.getcwd() # cwd current working directory
