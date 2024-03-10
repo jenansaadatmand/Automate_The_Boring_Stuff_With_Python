9d775238d6e4298cd9577d7b70a9459edbe15234
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-10 03:13:08 -0400

    Add files via upload

---
+++ hello World.py
@@ -0,0 +1,29 @@
+#print("Hello World!") # This prints some text
+#print("Jimmy is my Love")
+#red_bucket = input ("What do you want to put in the bucket?")
+#print(red_bucket)
+#print(type(red_bucket))
+#print(5==4)
+#print(5!=4)
+#Thomas_Age = 3
+#Age_at_kindergarten = 5
+#print(Thomas_Age == Age_at_kindergarten)
+#print(Thomas_Age != Age_at_kindergarten)
+#if Thomas_Age < Age_at_kindergarten:
+#    print("Thomas should be in pre-school")
+#elif Thomas_Age == Age_at_kindergarten:
+#    print("Enjouy kindergraten")
+#else:
+#    print("Thomas should be in another class")
+# Creating a function, define and call a function
+def print_ken():
+    text = "ken Stro has a great channel"
+    print(text)
+    print(text)
+    print(text)
+print_ken()
+
+def print_kevin1(text1):
+    print(text1)
+
+print_kevin1("kevin stratvert has a goog channel")
