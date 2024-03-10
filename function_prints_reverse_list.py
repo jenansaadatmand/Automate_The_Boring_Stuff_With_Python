a64079359154467836fff6d0b95953ccc291f673
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-10 15:04:44 -0400

    Add files via upload

---
+++ function_prints_reverse_list.py
@@ -0,0 +1,18 @@
+list = [1,2,3,4,5,6,7,8,9,10]
+reverse_list = []
+print(list)
+n = len(list)-1
+while n >= 0:
+    reverse_list.append(list[n])
+    n -= 1
+print(reverse_list)
+
+print()
+
+def list(n):
+    result = [ ]
+    a, b = 1, 10
+    while n > 0 :
+        result.append(a)
+    return result
+
