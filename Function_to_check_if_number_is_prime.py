e58d88e2a1caef69db8354ae95862795a6166d42
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-11 12:45:01 -0400

    Add files via upload

---
+++ Function_to_check_if_number_is_prime.py
@@ -0,0 +1,11 @@
+#function defined to checks if number is prime
+def checkIfPrime (numberToCheck):
+    for x in range(2, numberToCheck):
+        if (numberToCheck%x == 0 ):
+            return False
+        return True
+# call the function
+checkIfPrime(13)
+# assign a variable to function
+#answer = checkIfPrime(13)
+#print(answer)
