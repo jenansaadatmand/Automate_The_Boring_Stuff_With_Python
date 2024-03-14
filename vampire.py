73000744e6d5bb2f2532a950217cc7c15066a313
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-14 10:52:58 -0400

    Add files via upload

---
+++ vampire.py
@@ -0,0 +1,9 @@
+if name == 'Alice':
+    print('Hi, Alice.')
+elif age < 12:
+    print('You are not Alice, Kiddo.')
+elif age > 2000:
+    print('unlike you, Alice is not an undead, immortal vampire.')
+elif age > 100:
+    print('You are not Alice, grannie.')
+ 
