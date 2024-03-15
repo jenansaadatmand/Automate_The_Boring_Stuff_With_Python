4c90493bb78146009c43781cdcce5abb1df290e5
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-15 00:25:13 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -42,7 +42,7 @@ message2 = "Local Variable"
 print("\nOUTSIDE THE FUNCTION")
 # Global variables are accessible outside function
 print(message1)
-#local variable are NOT accessible outside function
+# Local variable are NOT accessible outside function
 print(message2)
     
 print()
