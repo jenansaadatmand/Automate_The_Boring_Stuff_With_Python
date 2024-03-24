5c66f80f781d6f963648de09a1fc4babce2204ec
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-24 12:02:52 -0400

    Add files via upload

---
+++ Mary_game_ program.py
@@ -0,0 +1,10 @@
+input('Name:')
+name = input
+if name == 'Mary':
+    print('Hello Mary')
+input('password:')
+if password == 'swordfish':
+    print('Access granted.')
+else:
+    print('wrong password.')
+    
