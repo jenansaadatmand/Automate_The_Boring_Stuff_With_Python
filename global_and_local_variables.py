50cf42cdec6483e9a3ca0ba10b0341623b002b20
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-25 22:09:45 -0400

    Update global_and_local_variables.py

--- global_and_local_variables.py
+++ global_and_local_variables.py
@@ -6,7 +6,7 @@ def bacon():
     eggs = 'bacon local'
     print(eggs)    # Prints 'bacon local'
     spam()
-    print(eggs)    # prints 'bacon local'
+    print(eggs)    # Prints 'bacon local'
 eggs = 'global'
 bacon()
 print(eggs)        # prints 'global'
