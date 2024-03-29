d1ca5827c67c2b93a88244425343c227c4d44348
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-29 01:03:22 -0400

    Add files via upload

---
+++ printand keywordsarguments.py
@@ -0,0 +1,16 @@
+print('Hello')
+print('World')
+print()
+print('Hello', end='')
+print('World')
+print()
+print('Jimmy', end='')
+print('Jenan')
+print()
+print('cats', 'dogs', 'mice')
+print('cats', 'dogs', 'mice', sep=',')
+print('cats', ' dogs', ' mice', sep=',')
+print('cats', 'dogs', 'mice', sep='/')
+print('cats', ' dogs', ' mice', sep='')
+
+
