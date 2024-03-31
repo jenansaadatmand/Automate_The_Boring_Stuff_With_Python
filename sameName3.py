95ec6c1430479a0de13ee000e27b6de255d5af8a
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-31 11:08:08 -0400

    Update sameName3.py

--- sameName3.py
+++ sameName3.py
@@ -6,7 +6,7 @@ def bacon():
     eggs = 'bacon'   # This is a local
 
 def ham():
-    print(eggs)      # this is the global
+    print(eggs)      # This is the global
 
 eggs = 42            # this is the global
 spam()
