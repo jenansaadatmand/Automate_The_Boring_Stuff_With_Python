2d6ba35d0fb9a6114866cda9408d4703017281e3
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-04-02 01:48:29 -0400

    Add files via upload

---
+++ sameName4.py
@@ -0,0 +1,7 @@
+def spam():
+    print(eggs)    # Error! first need to assign a value for it
+    eggs = 'spam local'
+#    print(eggs)   # need to remove commenting out and delete top print(eggs)
+eggs = 'global'
+spam()
+print(eggs)
