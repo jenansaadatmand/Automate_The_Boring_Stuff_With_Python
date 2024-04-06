69ae372cb481979c95fb503b4bb15394b6b29ff6
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-04-06 12:13:06 -0400

    Update reading_and_writing_file_buffer_ size.py

--- reading_and_writing_file_buffer_ size.py
+++ reading_and_writing_file_buffer_ size.py
@@ -5,7 +5,7 @@ outputFile = open('myoutputfile.txt', 'w')
 msg = inputFile.read(10) # read 10 bytes only
 while len(msg):   # Loop through file 10 bytes at a time, checks len variable msg, as long as the value is not 0 the loop will run
     outputFile.write(msg + '\n')
-    msg = inputFile.read(10) #Writes a message to output file, after writing the message, a statement will read the next 10 bytes and keeps doing it until entire file is read
+    msg = inputFile.read(10) #Writes a message to output file, after writing the message, a statement will read the next 10 bytes and keeps doing it until the entire file is read
 inputFile.close()
 outputFile.close()
 
