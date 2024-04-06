2b8bde43b2da3a57c0e94358f705f4b5c7fb6a84
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-04-06 12:20:00 -0400

    Update reading_and_writing_file_buffer_ size.py

--- reading_and_writing_file_buffer_ size.py
+++ reading_and_writing_file_buffer_ size.py
@@ -13,7 +13,7 @@ outputFile.close()
 # Use 'rb' or 'wb' mode to read binary, write binary
 #inputFile = open('myimage.jpg', 'rb')
 #outputFile = open('myoutputimage.jpg', 'wb')
-#msg = inputFile.read(10) # read 10 bytes only
+#msg = inputFile.read(10) # Read 10 bytes only
 #while len(msg):   # Loop through file 10 bytes at a time, checks len variable msg, as long as the value is not 0 the oop will run
 #    outputFile.write(msg) msg = inputFile.read(10) #Writes a message to the output file, after writing the message, the statement will read the next 10 bytes and keeps doing it until entire file is read
 #inputFile.close()
