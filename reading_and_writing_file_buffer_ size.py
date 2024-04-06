8ee3e7f9baaf16656e8333ad06d63e46869113bc
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-04-06 12:14:49 -0400

    Update reading_and_writing_file_buffer_ size.py

--- reading_and_writing_file_buffer_ size.py
+++ reading_and_writing_file_buffer_ size.py
@@ -15,6 +15,6 @@ outputFile.close()
 #outputFile = open('myoutputimage.jpg', 'wb')
 #msg = inputFile.read(10) # read 10 bytes only
 #while len(msg):   # Loop through file 10 bytes at a time, checks len variable msg, as long as the value is not 0 the oop will run
-#    outputFile.write(msg) msg = inputFile.read(10) #writes a message to the output file, after writing the message, the statement will read the next 10 bytes and keeps doing it until entire file is read
+#    outputFile.write(msg) msg = inputFile.read(10) #Writes a message to the output file, after writing the message, the statement will read the next 10 bytes and keeps doing it until entire file is read
 #inputFile.close()
 #outputFile.close()
