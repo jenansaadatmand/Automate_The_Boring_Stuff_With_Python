809ac799be4c9e9f78fc43d9ae1b45413226c060
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-04-07 12:22:21 -0400

    Add files via upload

---
+++ lottomax.py
@@ -0,0 +1,17 @@
+# Program generates random 7 lotto numbers between 1 and 50
+import random
+a=[]
+for i in range(0, 7):
+    a.append(random.randint(1, 51))
+    print('Lottomax selection list is: ' ,a)    # print indented prints various lists
+
+print()
+
+import random
+a=[]
+for i in range(0, 7):
+    a.append(random.randint(1,51))
+print('Lottomax selection list is: ',a)
+
+
+   
