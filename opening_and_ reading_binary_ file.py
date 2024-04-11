4d26902bf9369234ccbb1d55ea98b3fa361ad773
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-04-10 20:44:49 -0400

    Update opening_and_ reading_binary_ file.py

--- opening_and_ reading_binary_ file.py
+++ opening_and_ reading_binary_ file.py
@@ -3,7 +3,7 @@
 inputFile = open('myimage.jpeg', 'rb')
 outputFile = open('myoutputimage.jpg', 'wb')
 msg = inputFile.read(10) # read 10 bytes only
-while len(msg):   # loop through file 10 bytes at a time, checks len variable msg, as long as value is not 0 the oop will run
+while len(msg):   # Loop through file 10 bytes at a time, checks len variable msg, as long as value is not 0 the oop will run
     outputFile.write(msg)
     msg = inputFile.read(10) #writes message to output file, after writing the message, statement will read the next 10 bytes and keeps doing it until entire file is read
 inputFile.close()
