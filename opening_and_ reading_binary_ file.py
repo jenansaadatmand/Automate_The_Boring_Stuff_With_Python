3de3923a01d04f972f53ae64dbb09b52d3c4167e
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-04-10 20:44:12 -0400

    Add files via upload

---
+++ opening_and_ reading_binary_ file.py
@@ -0,0 +1,11 @@
+#opening, reading and writing binary files (non text, image or video)
+# use 'rb' or 'wb' mode read binary, write binary
+inputFile = open('myimage.jpeg', 'rb')
+outputFile = open('myoutputimage.jpg', 'wb')
+msg = inputFile.read(10) # read 10 bytes only
+while len(msg):   # loop through file 10 bytes at a time, checks len variable msg, as long as value is not 0 the oop will run
+    outputFile.write(msg)
+    msg = inputFile.read(10) #writes message to output file, after writing the message, statement will read the next 10 bytes and keeps doing it until entire file is read
+inputFile.close()
+outputFile.close()
+
