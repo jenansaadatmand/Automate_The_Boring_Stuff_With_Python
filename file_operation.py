ef9f940cf07dfae8e6c2cbff68d9d218fbc753b8
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-04-11 18:41:19 -0400

    Add files via upload

---
+++ file_operation.py
@@ -0,0 +1,44 @@
+#Opening and reading Text files using readline() method, open(two parameters)
+f = open('myfile.txt', 'r') #open(filepath, mode r reading only)  
+firstline = f.readline() # reads first line and assign it to variable firstline
+secondline = f.readline() #reads second line and assign to variable second line
+thirdline = f.readline()
+fourthline = f.readline()
+fifthline = f.readline()
+print(firstline)
+print(secondline)
+print(thirdline, end = '')
+print(fourthline)
+print(fifthline)
+f.close() # closes the file to free up system resources
+# readline() function adds a new line '\n' after each line
+#if you don't want the extra new line between each line of text
+'''print(firstline, end = '')''' # single line no space
+print()
+
+# For Loop to read text files efficient way
+# for loop loops through text file line by line , read all file
+f = open('myfile.txt', 'r')
+for line in f:
+     print(line, end = '') # end = '' prints no single line
+f.close()
+
+# write to a file using a (append) mode
+#or 'W' write mode but this will erase all previous content in file if it exists
+# append two setences to file
+f = open('myfile.txt', 'a')
+f.write('\nTHIS sentence will be appended.') # escape character "\n" for new line
+f.write('\nPython is Fun!')
+print() # will print all text in file
+f.close()
+# if you keep running the program, it will append the samething over and over
+print()
+
+
+
+
+
+
+
+
+
