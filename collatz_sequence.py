5f96bf095674f52fb962b560f2a5657327ed6533
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-04-15 11:14:08 -0400

    Add files via upload

---
+++ collatz_sequence.py
@@ -0,0 +1,16 @@
+def collatz(n):
+    if n == 2:
+        return n // 2
+        a = n // 2
+        print(int(a))
+    elif n == 1:
+        return 3 * (n + 1)
+        b = 3 * (n + 1)
+        print(int(b))
+    else: 
+        return 0
+        print('number is neither even nor odd')
+
+n = int(input())
+collatz(n)
+ 
