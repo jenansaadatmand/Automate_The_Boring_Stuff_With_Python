a4fed908c98fbea16b09fbd76ba68a3fbd77f41c
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2025-08-31 01:17:17 -0400

    Update deleting_and renaming_files.py

--- deleting_and renaming_files.py
+++ deleting_and renaming_files.py
@@ -14,7 +14,6 @@ os.rename('newDir', 'newDir2') # Rename directory
 #time.sleep(2)
 #os.rmdir('newDir2') # Remove directory
 
-
 #rename('myfile.txt', 'nfile.txt')
 #file = open(nfile.text)
 #print(file)
