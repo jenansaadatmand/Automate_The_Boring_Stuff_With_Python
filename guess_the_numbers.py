bb8f58d1dbf883c86bf01063e3f45f52f1e35972
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2026-05-22 21:00:33 -0400

    Update guess_the_numbers.py

--- guess_the_numbers.py
+++ guess_the_numbers.py
@@ -8,8 +8,7 @@ print('I am thinking of a number between 1 and 20.')
 for guessesTaken in range(1, 6):
     print('Take a guess.')
     guess = int(input())
-
-    
+\  
     if guess < secretNumber:
         print('Your guess is too low.')
     elif guess > secretNumber:
