6032aebef8cd36e638c8e0f7571588ba6e3528f3
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2026-06-05 21:53:56 -0400

    Update magic8Ball.py

--- magic8Ball.py
+++ magic8Ball.py
@@ -2,7 +2,6 @@ import random
 def getAnswer(answerNumber):
     if answerNumber == 1:
         return 'It is certain'
-        
     elif answerNumber == 2:
         return 'It is decidedly so'
     elif answerNumber == 3:
